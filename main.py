ae9e467e9c705f73372f0b863cda71156a1d8cc5
sjksjsgwk <can01443161@gmail.com>
2025-12-31 14:30:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+from telethon import TelegramClient, events
+import asyncio
+import os
+
+# Railway Variables
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+
+# SADECE SEN
+OWNER_ID = 7051750193
+
+# Grup ID
+GROUP = -1001692874801
+
+client = TelegramClient("session", api_id, api_hash)
+
+running = False
+task = None
+
+async def spam_sil():
+    global running
+    while running:
+        msg = await client.send_message(GROUP, "..")
+        await asyncio.sleep(0.3)
+        await msg.delete()
+        await asyncio.sleep(10)
+
+@client.on(events.NewMessage(pattern="/baslat"))
+async def start_handler(event):
+    global running, task
+
+    if event.sender_id != OWNER_ID:
+        return
+
+    if running:
+        await event.reply("⚠️ Zaten çalışıyor.")
+        return
+
+    running = True
+    task = asyncio.create_task(spam_sil())
+    await event.reply("✅ Başlatıldı.")
+
+@client.on(events.NewMessage(pattern="/durdur"))
+async def stop_handler(event):
+    global running, task
+
+    if event.sender_id != OWNER_ID:
+        return
+
+    if not running:
+        await event.reply("⚠️ Zaten durmuş.")
+        return
+
+    running = False
+    if task:
+        task.cancel()
+        task = None
+
+    await event.reply("⛔ Durduruldu.")
+
+client.start()
+print("Userbot aktif (sadece sen kontrol edebilirsin)")
+client.run_until_disconnected()
